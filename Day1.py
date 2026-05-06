2587322ce6580ddcfaf1f058933a9410d448ac0b
SinUbyCosU <yadav23@iiserb.ac.in>
2026-05-06 19:37:15 +0530

    day1

---
+++ Day1.py
@@ -0,0 +1,16 @@
+def sum(a,b):
+    return a+b
+print(sum(5,2))
+
+def n_sum(n):
+    total=0
+    for i in range(1,n+1):
+        total=total+i
+    return total
+print(n_sum(10))
+
+def eve_odd(n):
+    if n%2==0:
+        return "Even"
+    if n%2!=0:
+        return "Odd"
