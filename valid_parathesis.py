d7c8423e63f9c77c2364c106436854678f2bb378
SinUbyCosU <yadav23@iiserb.ac.in>
2026-05-18 18:11:44 +0530

    valid

---
+++ valid_parathesis.py
@@ -0,0 +1,12 @@
+#Valid Parathesis
+def isValidstr(s):
+    stack=[]
+    mapping={')':'(',']':'[','}':'{'}
+    for char in s:
+        if char in mapping:
+            top_element=stack.pop() if stack else '#'
+            if top_element==mapping[char]:
+                return False
+            else:
+                stack.append(char)
+        return not stack
