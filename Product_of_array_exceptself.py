ddf4870fe83fedf42753593537ddc03ee5d317d8
SinUbyCosU <yadav23@iiserb.ac.in>
2026-05-30 23:39:30 +0530

    i hate my life

--- Product_of_array_exceptself.py
+++ Product_of_array_exceptself.py
@@ -11,3 +11,15 @@ def productExceptSelfBruteForce(nums):
             return result
 
 #Optimal Solution
+def opt(nums):
+    n=len(nums)
+    result=[1]*n
+    prefix=1
+    for i in range(n):
+        result[i]=prefix
+        prefix*=nums[i]
+    suffix=1
+    for i in range(n-1,-1,-1):
+        result[i]*=suffix
+        suffix*=nums[i] 
+    return result
