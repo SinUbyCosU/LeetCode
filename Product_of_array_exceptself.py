80be00245df12133d70a2654961eea414e14f423
SinUbyCosU <yadav23@iiserb.ac.in>
2026-05-30 22:15:56 +0530

    brute

---
+++ Product_of_array_exceptself.py
@@ -0,0 +1,11 @@
+#Brute Force
+def productExceptSelfBruteForce(nums):
+    result=[]
+    n=len(nums)
+    for i in range(n):
+        product=1
+        for j in range(n):
+            if i!=j:
+                product*=nums[j]
+                result.append(product)
+            return result
